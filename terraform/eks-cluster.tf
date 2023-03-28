9fcd6d5eb20006f3eb609f40dc8a0db4bd733c95
Lilian Anya Galadima <104821662+Lily-G1@users.noreply.github.com>
2023-03-28 18:12:14 +0100

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -44,10 +44,38 @@ module "eks" {
     eks_managed_node_groups = {
         dev = {
             min_size = 1
+            max_size = 2
+            desired_size = 1
+
+            instance_types = ["t2.micro"]
+        }
+    }
+}
+
+        
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0" 
+    cluster_name = "sockshop-eks-cluster"
+    cluster_version = "1.25"
+
+    cluster_endpoint_public_access  = true
+
+    vpc_id = module.webapp-vpc.vpc_id
+    subnet_ids = module.webapp-vpc.private_subnets
+
+    tags = {
+        environment = "development"
+        application = "sockshop"
+    }
+
+    eks_managed_node_groups = {
+        dev = {
+            min_size = 1
             max_size = 3
             desired_size = 2
 
-            instance_types = ["t2.micro"]
+            instance_types = ["t2.medium"]
         }
     }
 }
