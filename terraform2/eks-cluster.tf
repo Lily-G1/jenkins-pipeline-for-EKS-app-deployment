e3d0bd47f6ce348c73cd45be89631c6c0e72a6ec
Lilian Anya Galadima <104821662+Lily-G1@users.noreply.github.com>
2023-03-29 19:52:53 +0100

    Add files via upload

---
+++ terraform2/eks-cluster.tf
@@ -0,0 +1,114 @@
+#
+# EKS Cluster Resources
+#  * IAM Role to allow EKS service to manage other AWS services
+#  * EC2 Security Group to allow networking traffic with EKS cluster
+#  * EKS Cluster
+#
+
+resource "aws_iam_role" "demo-cluster" {
+  name = "terraform-eks-demo-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.demo-cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.demo-cluster.name
+}
+
+resource "aws_security_group" "demo-cluster" {
+  name        = "terraform-eks-demo-cluster"
+  # description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.demo.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-eks-demo"
+  }
+}
+
+resource "aws_security_group_rule" "demo-cluster-HTTPS" {
+  # cidr_blocks       = [local.workstation-external-cidr]
+  cidr_blocks       = ["0.0.0.0/0"]
+  # description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.demo-cluster.id
+  to_port           = 443
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "demo-cluster-HTTP" {
+  # cidr_blocks       = [local.workstation-external-cidr]
+  cidr_blocks       = ["0.0.0.0/0"]
+  # description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.demo-cluster.id
+  to_port           = 80
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "demo-cluster-8080" {
+  # cidr_blocks       = [local.workstation-external-cidr]
+  cidr_blocks       = ["0.0.0.0/0"]
+  # description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 8080
+  protocol          = "tcp"
+  security_group_id = aws_security_group.demo-cluster.id
+  to_port           = 8080
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "demo-cluster-SSH" {
+  # cidr_blocks       = [local.workstation-external-cidr]
+  cidr_blocks       = ["0.0.0.0/0"]
+  # description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.demo-cluster.id
+  to_port           = 22
+  type              = "ingress"
+}
+
+resource "aws_eks_cluster" "demo" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.demo-cluster.arn
+  version = "1.25"
+
+  vpc_config {
+    security_group_ids = [aws_security_group.demo-cluster.id]
+    subnet_ids         = aws_subnet.demo[*].id
+    endpoint_private_access = true
+    endpoint_public_access = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.demo-cluster-AmazonEKSVPCResourceController,
+  ]
+}
---
+++ terraform2/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  default = "sockshop-eks-demo"
+  type    = string
+}
+
+variable "node_ami_type" {
+  default = "AL2_x86_64"
+  type    = string
+}
+
+# variable "instance_type" {
+#   default = ["t3.medium"]
+#   type    = string
+# }
