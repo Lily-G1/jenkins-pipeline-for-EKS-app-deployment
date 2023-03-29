1cbe8c4b387298c3e734a37b849fceea23d17344
Lilian Anya Galadima <104821662+Lily-G1@users.noreply.github.com>
2023-03-29 02:06:42 +0100

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -25,32 +25,32 @@
 #     }
 # }
 
-module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
-    cluster_name = "webapp-eks-cluster"
-    cluster_version = "1.25"
+# module "eks" {
+#     source  = "terraform-aws-modules/eks/aws"
+#     version = "~> 19.0"
+#     cluster_name = "webapp-eks-cluster"
+#     cluster_version = "1.25"
 
-    cluster_endpoint_public_access  = true
+#     cluster_endpoint_public_access  = true
 
-    vpc_id = module.webapp-vpc.vpc_id
-    subnet_ids = module.webapp-vpc.private_subnets
+#     vpc_id = module.webapp-vpc.vpc_id
+#     subnet_ids = module.webapp-vpc.private_subnets
 
-    tags = {
-        environment = "development"
-        application = "webapp"
-    }
+#     tags = {
+#         environment = "development"
+#         application = "webapp"
+#     }
 
-    eks_managed_node_groups = {
-        dev = {
-            min_size = 1
-            max_size = 3
-            desired_size = 2
+#     eks_managed_node_groups = {
+#         dev = {
+#             min_size = 1
+#             max_size = 3
+#             desired_size = 2
 
-            instance_types = ["t2.micro"]
-        }
-    }
-}
+#             instance_types = ["t2.micro"]
+#         }
+#     }
+# }
 
         
 module "eks2" {
