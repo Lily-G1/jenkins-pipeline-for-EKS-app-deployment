37e27b679b213baeecadc89551438b4b9a3ef242
Lilian Anya Galadima <104821662+Lily-G1@users.noreply.github.com>
2023-03-31 21:37:21 +0100

    Update main.tf

--- prometheus-grafana/main.tf
+++ prometheus-grafana/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "eks_cluster_monitoring" {
     provisioner "local-exec" {
-        command = "sh /eks_monitoring_provision.sh ${aws_eks_cluster.demo.id}"
+        command = "sh /eks_monitoring_provision.sh ${aws_eks_cluster.sockshop-eks-demo.id}"
     }
     # depends_on = [
     #     "${aws_eks_cluster.master-node.name}"
