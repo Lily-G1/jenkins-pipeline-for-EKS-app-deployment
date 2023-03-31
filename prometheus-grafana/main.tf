5457568f11310ba5a156410826d3d3f9654cfea9
Lilian Anya Galadima <104821662+Lily-G1@users.noreply.github.com>
2023-03-31 21:54:41 +0100

    Update main.tf

--- prometheus-grafana/main.tf
+++ prometheus-grafana/main.tf
@@ -1,6 +1,24 @@
+# resource "null_resource" "eks_cluster_monitoring" {
+#     provisioner "local-exec" {
+#         command = "sh /eks_monitoring_provision.sh ${aws_eks_cluster.sockshop-eks-demo.cluster_id}"
+#     }
+#     # depends_on = [
+#     #     "${aws_eks_cluster.master-node.name}"
+#     # ]
+# }
+
+
+data "aws_eks_cluster" "cluster_id" {
+  name = "sockshop-eks-demo"
+}
+
+# output "id" {
+#   value = data.aws_eks_cluster.sockshop-eks-demo.id
+# }
+
 resource "null_resource" "eks_cluster_monitoring" {
     provisioner "local-exec" {
-        command = "sh /eks_monitoring_provision.sh ${aws_eks_cluster.sockshop-eks-demo.cluster_id}"
+        command = "sh /eks_monitoring_provision.sh ${data.aws_eks_cluster.cluster_id.id}"
     }
     # depends_on = [
     #     "${aws_eks_cluster.master-node.name}"
