24ecd6c547fa1e07c03aa2c6897f7dfab9592dc2
Lilian Anya Galadima <104821662+Lily-G1@users.noreply.github.com>
2023-04-01 10:20:59 +0100

    Update eks_monitoring_provision.sh

--- prometheus-grafana/eks_monitoring_provision.sh
+++ prometheus-grafana/eks_monitoring_provision.sh
@@ -6,10 +6,12 @@
 # sudo ./get_helm.sh
 
 # CONFIGURE PROMETHEUS
-kubectl create namespace monitoring 
+# kubectl create namespace monitoring 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring
+kubectl get pods -n monitoring
+kubectl get svc -n monitoring
 kubectl port-forward svc/prometheus-kube-prometheus-prometheus -n monitoring 9090
 
 # CONFIGURE GRAFANA
