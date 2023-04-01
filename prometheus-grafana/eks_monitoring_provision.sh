0ceec40757719edc4636fd9972ca33ec57fd9992
Lilian Anya Galadima <104821662+Lily-G1@users.noreply.github.com>
2023-04-01 09:08:01 +0100

    Update eks_monitoring_provision.sh

--- prometheus-grafana/eks_monitoring_provision.sh
+++ prometheus-grafana/eks_monitoring_provision.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# INSTALL HELM
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-sudo chmod 700 get_helm.sh
-sudo ./get_helm.sh
+# # INSTALL HELM
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+# sudo chmod 700 get_helm.sh
+# sudo ./get_helm.sh
 
 # CONFIGURE PROMETHEUS
 kubectl create namespace monitoring 
